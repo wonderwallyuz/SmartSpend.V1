9817797045b41278bc2bb654d82c8024240a5eb3
V3ntong <mugiwaranomelvin@gmail.com>
2025-10-03 14:00:25 +0800

    icons, budget

--- Backend/init_db.py
+++ Backend/init_db.py
@@ -8,13 +8,20 @@ c = conn.cursor()
 
 # Recreate table with id as primary key
 c.execute("""
-CREATE TABLE IF NOT EXISTS expenses (
+    CREATE TABLE IF NOT EXISTS expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     description TEXT NOT NULL,
     amount REAL NOT NULL,
     date TEXT NOT NULL
-)
-""")
+)""")
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS budgettbl (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    period TEXT NOT NULL,
+    amount REAL NOT NULL,
+    date TEXT NOT NULL
+)""")
 
 conn.commit()
 conn.close()
